e506ac3a6dd3a6dd8b01155a37820153ca8a786a
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-10-20 09:45:21 +0000

    [WIP] fixing error on parent/notes/index

--- app/controllers/parent/homes_controller.rb
+++ app/controllers/parent/homes_controller.rb
@@ -1,12 +1,6 @@
 class Parent::HomesController < ApplicationController
-  before_action :set_parent
 
   def index
   end
 
-  private
-
-  def set_parent
-    @parent = Parent.find(params[:parent_id])
-  end
 end
--- app/controllers/parent/notes_controller.rb
+++ app/controllers/parent/notes_controller.rb
@@ -1,22 +1,21 @@
 class Parent::NotesController < ApplicationController
-  before_action :set_parent
   before_action :set_note, only: [:show, :edit, :update, :destroy]
 
   def index
-    @notes = @parent.notes
+    @notes = current_parent.notes
   end
 
   def show
   end
 
   def new
-    @note = @parent.notes.build
+    @note = current_parent.notes.build
   end
 
   def create
-    @note = @parent.notes.build(note_params)
+    @note = current_parent.notes.build(note_params)
     if @note.save
-      redirect_to parent_notes_path(@parent), notice: '連絡帳を作成しました'
+      redirect_to parent_notes_path, notice: '連絡帳を作成しました'
     else
       render :new
     end
@@ -27,7 +26,7 @@ class Parent::NotesController < ApplicationController
 
   def update
     if @note.update(note_params)
-      redirect_to parent_note_path(@parent, @note), notice: '連絡帳を更新しました'
+      redirect_to parent_note_path(@note), notice: '連絡帳を更新しました'
     else
       render :edit
     end
@@ -35,17 +34,13 @@ class Parent::NotesController < ApplicationController
 
   def destroy
     @note.destroy
-    redirect_to parent_notes_path(@parent), notice: '連絡帳を削除しました'
+    redirect_to parent_notes_path, notice: '連絡帳を削除しました'
   end
 
   private
 
-  def set_parent
-    @parent = Parent.find(params[:parent_id])
-  end
-
   def set_note
-    @note = @parent.notes.find(params[:id])
+    @note = current_parent.notes.find(params[:id])
   end
 
   def note_params
