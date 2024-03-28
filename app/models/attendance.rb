df373c7f5453721bd1ed576d69dd3357910f7cd1
KeiMonden <129693543+kei9625@users.noreply.github.com>
2024-03-28 11:54:29 +0000

    時計のtimezoneを変更。編集後のリダイレクト先を一覧に変更。

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,4 +2,13 @@ class Attendance < ApplicationRecord
   belongs_to :kid
   belongs_to :parent, optional: true
   belongs_to :employee, optional: true
+
+  def formatted_drop_off
+    self.drop_off.strftime("%Y/%m/%d %H:%M") if self.drop_off.present?
+  end
+
+  def formatted_pick_up
+    self.pick_up.strftime("%Y/%m/%d %H:%M") if self.pick_up.present?
+  end
+
 end
